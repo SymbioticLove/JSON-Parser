cd15344e3a4832f899f39fc88d26433a012be157
Symbiotic Love <matthew@symbiotic.love>
2023-08-11 12:03:47 -0700

    Completed
    Finally. This was maybe the hardest thing I've yet done.

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# JSON-Parser
- A simple JSON parser in JS. This is an exceptionally tricky task that remains ongoing. It correctly identifies properly formatted JSON data, but only identifies incorrectly formatted data ~70% of the time.
+# JSON Parser
+
+This script provides functionality to validate JSON files in a given directory.
+
+## Functions
+
+### `lexer(input)`
+Takes a JSON string and returns an array of tokens. If an unescaped control character is found, it returns `null`.
+
+### `validateNumber(token)`
+Returns `true` if the token is a valid JSON number.
+
+### `parseValue(tokens, depth)`
+Parses a value from the token list and returns `true` if it's valid.
+
+### `parseObject(tokens, depth)`
+Parses an object from the token list, returning `true` if it's valid.
+
+### `parseArray(tokens, depth)`
+Parses an array from the token list, returning `true` if it's valid.
+
+### `main(input)`
+The main validation function that checks if the input is a valid JSON. Returns `0` if valid and `1` if invalid.
+
+### `processFile(filePath)`
+Reads the file at the given path and passes its content to `main(input)` for validation.
+
+### `testDirectory(directoryPath)`
+Reads all files in the specified directory and checks them for valid JSON using `processFile(filePath)`.
+
+## Constants
+
+- `MAX_DEPTH`: Controls the maximum allowed nesting depth for objects and arrays.
+
+## Usage
+
+The script will process all files in the `test/` directory and print whether they are valid or invalid JSON.
--- parser.js
+++ parser.js
@@ -1,122 +1,158 @@
-const fs = require('fs');
+const fs = require("fs");
 
 function lexer(input) {
-  const regex = /"(?:\\.|[^"])*"|true|false|null|(?:-?\d+(?:\.\d+)?(?:[eE][+-]?\d+)?)|[\{\}\[\]:,]/g;
-  return input.match(regex) || [];
-}
+    const regex = /"(?:\\.|[^"])*"|true|false|null|(?:-?\d+(?:\.\d+)?(?:[eE][+-]?\d+)?)|[{}[\]:,]|\S+/g;
+    const tokens = input.match(regex) || [];
 
+    // Check for unescaped control characters in strings
+    for (const token of tokens) {
+        // eslint-disable-next-line no-control-regex
+        if (token.startsWith("\"") && /[\x00-\x1F\x7F]/.test(token.slice(1, -1).replace(/\\./g, ""))) {
+            console.log("Debug (Lexer): Unescaped control character in string:", token);
+            return null; // Indicate invalid input
+        }
+    }
+
+    return tokens;
+}
 
 function validateNumber(token) {
-  return /^-?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+-]?\d+)?$/.test(token);
+    return /^-?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+-]?\d+)?$/.test(token);
 }
 
-function parseValue(tokens) {
-  const token = tokens.shift();
+function parseValue(tokens, depth = 0) {
+    const token = tokens.shift();
 
-  if (token === '{') return parseObject(tokens);
-  if (token === '[') return parseArray(tokens);
-  if (token === 'true' || token === 'false' || token === 'null') return true;
-  if (validateNumber(token)) return true;
+    if (token === "{") return parseObject(tokens, depth + 1);
+    if (token === "[") return parseArray(tokens, depth + 1);
+    if (token === "true" || token === "false" || token === "null") return true;
+    if (validateNumber(token)) return true;
 
-  // Check if the token is a valid string
-  if (token.startsWith('"') && token.endsWith('"')) {
-    const inner = token.slice(1, -1);
-    if (/^(?:[^"\\]|\\["\\/bfnrt]|\\u[0-9a-fA-F]{4})*$/.test(inner)) {
-      return true;
-    } else {
-      console.log('Debug: Invalid string:', token);
-      return false;
+    // Check if the token is a valid string
+    if (token.startsWith("\"") && token.endsWith("\"")) {
+        const inner = token.slice(1, -1);
+        if (/^(?:[^"\\]|\\["\\/bfnrt]|\\u[0-9a-fA-F]{4})*$/.test(inner)) {
+            return true;
+        } else {
+            console.log("Debug: Invalid string:", token);
+            return false;
+        }
     }
-  }
 
-  console.log('Debug: Unexpected token:', token);
-  return false;
+    console.log("Debug: Unexpected token:", token);
+    return false;
 }
 
-function parseObject(tokens) {
-  if (tokens[0] === '}') {
-    tokens.shift(); // Remove closing brace
-    return true;
-  }
+const MAX_DEPTH = 19;
 
-  while (tokens.length) {
-    const key = tokens.shift();
-    if (!key.startsWith('"') || !key.endsWith('"')) {
-      console.log('Debug (Object): Expected string key.');
-      return false;
-    }
-    if (tokens.shift() !== ':') {
-      console.log('Debug (Object): Expected colon.');
-      return false;
-    }
-    if (!parseValue(tokens)) return false; // Parse value
-
-    if (tokens[0] === '}') {
-      tokens.shift(); // Remove closing brace
-      return true;
+function parseObject(tokens, depth = 0) {
+    if (depth > MAX_DEPTH) {
+        console.log("Debug: Nested objects too deep");
+        return false;
     }
 
-    if (tokens.shift() !== ',') {
-      console.log('Debug (Object): Expected comma or closing brace.');
-      return false;
+    if (tokens[0] === "}") {
+        tokens.shift(); // Remove closing brace
+        return true;
     }
-  }
 
-  return false;
+    while (tokens.length) {
+        const key = tokens.shift();
+        if (!key.startsWith("\"") || !key.endsWith("\"")) {
+            console.log("Debug (Object): Expected string key.");
+            return false;
+        }
+        if (tokens.shift() !== ":") {
+            console.log("Debug (Object): Expected colon.");
+            return false;
+        }
+        if (!parseValue(tokens, depth)) return false; // Parse value
+
+        if (tokens[0] === "}") {
+            tokens.shift(); // Remove closing brace
+            return true;
+        }
+
+        if (tokens.shift() !== ",") {
+            console.log("Debug (Object): Expected comma or closing brace.");
+            return false;
+        }
+    }
+
+    return false;
 }
 
-function parseArray(tokens) {
-  if (tokens[0] === ']') {
-    tokens.shift(); // Remove closing bracket
-    return true;
-  }
-
-  while (tokens.length) {
-    if (!parseValue(tokens)) return false; // Parse value
-
-    if (tokens[0] === ']') {
-      tokens.shift(); // Remove closing bracket
-      return true;
+function parseArray(tokens, depth = 0) {
+    if (depth > MAX_DEPTH) {
+        console.log("Debug: Nested arrays too deep");
+        return false;
     }
 
-    if (tokens.shift() !== ',') {
-      console.log('Debug (Array): Expected comma or closing bracket.');
-      return false;
+    if (tokens[0] === "]") {
+        tokens.shift(); // Remove closing bracket
+        return true;
     }
-  }
 
-  return false;
+    while (tokens.length) {
+        if (!parseValue(tokens, depth)) return false; // Parse value
+
+        if (tokens[0] === "]") {
+            tokens.shift(); // Remove closing bracket
+            return true;
+        }
+
+        if (tokens.shift() !== ",") {
+            console.log("Debug (Array): Expected comma or closing bracket.");
+            return false;
+        }
+    }
+
+    return false;
 }
 
 function main(input) {
-  const tokens = lexer(input);
-  const isValid = parseValue(tokens);
+    const tokens = lexer(input);
+    if (tokens === null) {
+        console.log("Invalid JSON");
+        return 1; // Invalid
+    }
 
-  if (isValid && tokens.length === 0) {
-    console.log("Valid JSON");
-    return 0; // Valid
-  } else {
-    console.log("Invalid JSON");
-    return 1; // Invalid
-  }
+    const firstToken = tokens[0];
+
+    // Check for invalid tokens (i.e., tokens that don't match any valid pattern)
+    if (tokens.some(token => !/^(?:"(?:\\.|[^"])*"|true|false|null|(?:-?\d+(?:\.\d+)?(?:[eE][+-]?\d+)?)|[{}[\]:,])$/.test(token))) {
+        console.log("Invalid JSON");
+        return 1; // Invalid
+    }
+  
+    const isValid = parseValue(tokens);
+
+    // Check if the top-level payload is an object or array
+    if (isValid && tokens.length === 0 && (firstToken === "{" || firstToken === "[")) {
+        console.log("Valid JSON");
+        return 0; // Valid
+    } else {
+        console.log("Invalid JSON");
+        return 1; // Invalid
+    }
 }
 
 function processFile(filePath) {
-  const input = fs.readFileSync(filePath, 'utf8');
-  return main(input);
+    const input = fs.readFileSync(filePath, "utf8");
+    return main(input);
 }
 
 function testDirectory(directoryPath) {
-  const files = fs.readdirSync(directoryPath);
-  for (const file of files) {
-    console.log(`Processing file: ${file}`);
-    const resultCode = processFile(`${directoryPath}/${file}`);
-    if (resultCode === 0) {
-      console.log(`${file} is Valid JSON\n`);
-    } else {
-      console.log(`${file} is Invalid JSON\n`);
+    const files = fs.readdirSync(directoryPath);
+    for (const file of files) {
+        console.log(`Processing file: ${file}`);
+        const resultCode = processFile(`${directoryPath}/${file}`);
+        if (resultCode === 0) {
+            console.log(`${file} is Valid JSON\n`);
+        } else {
+            console.log(`${file} is Invalid JSON\n`);
+        }
     }
-  }
 }
 
-testDirectory('test/');
+testDirectory("test/");
